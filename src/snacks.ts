3c0895d0103c1cf5597e155639d408a456004d19
hacked1123 <captaindarsh32@gmail.com>
2025-10-15 16:45:36 -0700

    added trail mix

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,6 +1,6 @@
 
 import {bold} from "./animation.ts";
-const snacks: string[] = ["chips","cookies","muffins"];
+const snacks: string[] = ["chips","cookies","muffins","trail mix"];
 export function printSnacks(): void { 
    for(const snack of snacks) {
 	console.log(`- ${snack}`);
