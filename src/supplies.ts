003687ac507e7bf41525c6a4591488e0ae569d62
Sameera Agarwala <sameera.agarwala@gmail.com>
2025-10-14 19:34:34 -0700

    Added new feature

--- src/supplies.ts
+++ src/supplies.ts
@@ -3,7 +3,8 @@ const supplies = ["cups", "plates", "streamers", "table"];
 
 // Function for printing supplies
 export function printSupplies() {
-  console.log(supplies);
+    console.log(`\x1b[1mThese are the supplies!\x1b[0m`); // Bold
+    console.log(supplies);
 }
 
 // Print supplies
