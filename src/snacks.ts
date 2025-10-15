f06829b33a298ae8a0df5750a62fe4108f484b06
Sameera Agarwala <sameera.agarwala@gmail.com>
2025-10-14 22:11:36 -0700

    Added all final snacks

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,6 +1,7 @@
 import {bold} from './animation';
 
-const snacks: string[] = ["chips","cookies","muffins"];
+const snacks: string[] = ["chips","cookies","muffins","strawberries","cheetos","brownies"];
+
 export function printSnacks(): void { 
    for(const snack of snacks) {
 	console.log(`- ${snack}`);
