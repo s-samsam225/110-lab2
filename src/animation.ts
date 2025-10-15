bc48e275065af631bd6e8d20d33e59921b4b7940
Sameera Agarwala <sameera.agarwala@gmail.com>
2025-10-14 19:48:12 -0700

    Added new feature

---
+++ src/animation.ts
@@ -0,0 +1,3 @@
+export function bold() {
+    console.log(`\x1b[1mThese are the supplies!\x1b[0m`); // Bold
+}
--- src/supplies.ts
+++ src/supplies.ts
@@ -1,11 +1,14 @@
+// Import feature function
+import {bold} from './animation.ts';
+
 // Define array of supplies
 const supplies = ["cups", "plates", "streamers", "table"];
 
 // Function for printing supplies
 export function printSupplies() {
-    console.log(`\x1b[1mThese are the supplies!\x1b[0m`); // Bold
     console.log(supplies);
 }
 
 // Print supplies
+bold();
 printSupplies();
