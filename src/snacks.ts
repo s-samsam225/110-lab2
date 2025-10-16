39f86066ef759cdc7cd2c3f3812b4c837d5ed537
Sameera Agarwala <sameera.agarwala@gmail.com>
2025-10-15 18:47:55 -0700

    Fixed typo in snacks list

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,7 +1,7 @@
 
 
 import {bold} from "./animation.ts";
-const snacks: string[] = ["chips","cookies","muffins","trail mix","fruit cups","candy","strawberries","cheetos,"brownies"];
+const snacks: string[] = ["chips","cookies","muffins","trail mix","fruit cups","candy","strawberries","cheetos","brownies"];
 >>>>>>> feature/add-snacks
 export function printSnacks(): void { 
    for(const snack of snacks) {
