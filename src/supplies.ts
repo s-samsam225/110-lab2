c65bee1f06bc8e40c6cb6e3860a0d7f8199ff830
hacked1123 <captaindarsh32@gmail.com>
2025-10-15 19:58:32 -0700

    fixed erros

--- src/supplies.ts
+++ src/supplies.ts
@@ -2,7 +2,7 @@
 import {bold} from './animation';
 
 // Define array of supplies
-const supplies = ["cups", "plates", "streamers", "table", "chairs"];
+export const supplies = ["cups", "plates", "streamers", "table", "chairs"];
 
 // Function for printing supplies
 export function printSupplies() {
