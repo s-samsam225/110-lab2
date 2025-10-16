ef82a256294b83cf3aa7f98597d4097ba6d75e21
Sameera Agarwala <sameera.agarwala@gmail.com>
2025-10-15 18:48:40 -0700

    Removed feature line in snacks.ts

--- src/snacks.ts
+++ src/snacks.ts
@@ -2,7 +2,6 @@
 
 import {bold} from "./animation.ts";
 const snacks: string[] = ["chips","cookies","muffins","trail mix","fruit cups","candy","strawberries","cheetos","brownies"];
->>>>>>> feature/add-snacks
 export function printSnacks(): void { 
    for(const snack of snacks) {
 	console.log(`- ${snack}`);
