9bfd3aec9f0c9d400bd52405433eb5729f35c221
hacked1123 <captaindarsh32@gmail.com>
2025-10-15 19:35:08 -0700

    fixed errors

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,6 +1,6 @@
 
-import {bold} from "./animation.ts";
-const snacks: string[] = ["chips","cookies","muffins","trail mix","fruit cups","candy"];
+import {bold} from "./animation";
+export const snacks: string[] = ["chips","cookies","muffins","trail mix","fruit cups","candy"];
 export function printSnacks(): void { 
    for(const snack of snacks) {
 	console.log(`- ${snack}`);
